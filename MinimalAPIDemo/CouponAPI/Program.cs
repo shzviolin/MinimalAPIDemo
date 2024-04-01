51786171480a73297007d94db908f4faaa945f6e
Ali Akbar Ebrahimi <a.ebrahimi1367@gmail.com>
2024-04-01 18:27:08 +0330

    03_Implement GetCoupon_GetCouponById_CreateCoupon

--- MinimalAPIDemo/CouponAPI/Program.cs
+++ MinimalAPIDemo/CouponAPI/Program.cs
@@ -1,4 +1,6 @@
 using CouponAPI.Data;
+using CouponAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,8 +18,38 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/api/coupon", () => Results.Ok(CouponStore.CouponList));
+//app.MapGet("/api/coupon", () => Results.Ok(CouponStore.CouponList)); 
+app.MapGet("/api/coupon", () =>
+{
+    return Results.Ok(CouponStore.CouponList);
+}).WithName("GetCoupons")
+  .Produces<IEnumerable<Coupon>>(200);
 
+app.MapGet("/api/coupon/{id:int}", (int id) =>
+{
+    return Results.Ok(CouponStore.CouponList.FirstOrDefault(x => x.Id == id));
+}).WithName("GetCoupon")
+  .Produces<Coupon>(200);
+
+app.MapPost("/api/coupon", ([FromBody] Coupon coupon) =>
+{
+    if (coupon.Id != 0 || string.IsNullOrEmpty(coupon.Name))
+        return Results.BadRequest("Invalid Id or Coupon Name");
+
+    if (CouponStore.CouponList.Any(x => x.Name.ToLower() == coupon.Name.ToLower()))
+        return Results.BadRequest("Coupon Name already exists");
+
+    coupon.Id = CouponStore.CouponList.OrderByDescending(x => x.Id).FirstOrDefault().Id + 1;
+    CouponStore.CouponList.Add(coupon);
+
+    //return Results.Ok(coupon);
+    //return Results.Created($"/api/coupon/{coupon.Id}", coupon);
+    return Results.CreatedAtRoute("GetCoupon", new { id = coupon.Id }, coupon);
+
+}).WithName("CreateCoupon")
+  .Accepts<Coupon>("application/json")
+  .Produces<Coupon>(201)
+  .Produces(400);
 
 
 
